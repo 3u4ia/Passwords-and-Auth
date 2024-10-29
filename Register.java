539145585cffbdf8003c4897e39f0972e6fe5745
Elroi Belete <elroibelete777@gmail.com>
2024-10-28 23:08:48 -0500

    checkIfValidEntries couldn't make allow list. But now returns boolean instead of changing a class member.

--- Register.java
+++ Register.java
@@ -1,14 +1,16 @@
 public class Register {
     String username;
     String password;
-    boolean isEntryValid = false;
+    boolean isEntryValid = false; // I don't know if this is a good idea or not
 
     public Register(String username, String password){
         this.username = username;
         this.password = password;
     }
     void plainText(){
+        if(checkIfValidEntries()){
 
+        }
     }
 
 
@@ -16,14 +18,12 @@ public class Register {
 
 
 
-    public void checkIfValidEntries(){
+    public boolean checkIfValidEntries(){
         char tempChar;
-        boolean tempBool = false;
         if(username.length() > 10){
             System.out.println("Your username is too long.");
+            return false;
             // tempBool is already false;
-        } else {
-            tempBool = true;
         }
 
         for(int i = 0; i < username.length(); i++){
@@ -32,19 +32,13 @@ public class Register {
             // Check  if the ! actually negates the boolean
             if(Character.isUpperCase(tempChar)){
                 System.out.println("Make sure there are no capitals in your usernames");
-                if(tempBool){
-                    tempBool = false;
-                }
+                return false;
             }
             if(Character.isDigit(tempChar)) {
                 System.out.println("Make sure there aren't any numbers in your username");
-                if(tempBool){
-                    tempBool = false;
-                }
+                return false;
             }
         }
-
-        isEntryValid = tempBool;
-
+        return true;
     }
 }
